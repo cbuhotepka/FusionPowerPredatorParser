80463509111e465dfba08131550735853c5eeb34
cbuhotepka <cbuhotepka@gmail.com>
2021-09-18 15:18:43 +0300

    fix old bug

--- engine_modul/file_handler.py
+++ engine_modul/file_handler.py
@@ -126,7 +126,11 @@ class FileHandler:
         _counter = Counter()
         pat = re.compile(f'{self.delimiter}')
         self.file.open()
-        lines = [self.file.readline() for _ in range(25)]
+        lines = []
+        for i, line in enumerate(self.file):
+            lines.append(line)
+            if i == 25:
+                break
         n = 0 if len(lines) < 10 else 5
         for i, line in enumerate(lines):
             if i >= n and (line != '' or line != '\n'):
