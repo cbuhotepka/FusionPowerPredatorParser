1ccc5b5ef0f679e94d38cfd642686feae62845e4
cbuhotepka <cbuhotepka@gmail.com>
2022-06-18 15:40:15 +0300

    refactor(delimiter_finder): Remove print

--- engine_modul/utils/delimiter.py
+++ engine_modul/utils/delimiter.py
@@ -6,7 +6,5 @@ def find_delimiter(list_lines: list[str]):
     for line in list_lines:
         for delimiter in POSSIBLE_DELIMITERS:
             counts[delimiter] += line.count(delimiter)
-    print(counts)
     sorted_counts = sorted([delimiter for delimiter in counts.keys()], key=lambda d: counts[d])
-    print(sorted_counts)
     return sorted_counts[-1]
