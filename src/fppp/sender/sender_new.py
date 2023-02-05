1b7d09f38ebbf009e5c5aa0ea77a0615974855d7
tm.kk23 <mansur.tumashev@gmail.com>
2023-02-06 00:47:08 +0500

    fixed sender_new, relative path to config

--- src/fppp/sender/sender_new.py
+++ src/fppp/sender/sender_new.py
@@ -11,7 +11,7 @@ import re
 import json
 from py_dotenv import read_dotenv
 
-dotenv_path = Path(__file__).parent / 'CONFIG.env'
+dotenv_path = Path(__file__).parent.parent / 'CONFIG.env'
 assert dotenv_path.exists()
 read_dotenv(dotenv_path)
 
