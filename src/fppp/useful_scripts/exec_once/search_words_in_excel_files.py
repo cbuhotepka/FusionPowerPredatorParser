8b522ff3d46f7ba7466d29a470858f5f84c19d6c
tm.kk23 <mansur.tumashev@gmail.com>
2023-02-18 17:49:04 +0500

    add search_words_in_excel_files.py

---
+++ src/fppp/useful_scripts/exec_once/search_words_in_excel_files.py
@@ -0,0 +1,50 @@
+import os
+import openpyxl
+
+
+def search_words_in_files(folder_path, words_list):
+    # Перебираем все файлы в папке
+    for root, dirs, files in os.walk(folder_path):
+        for file_name in files:
+            # Получаем полный путь до файла
+            file_path = os.path.join(root, file_name)
+            # Ищем нужные слова в файле Excel
+            if file_name.endswith('.xlsx') or file_name.endswith('.xlsm'):
+                workbook = openpyxl.load_workbook(file_path)
+                for sheet_name in workbook.sheetnames:
+                    sheet = workbook[sheet_name]
+                    for row in sheet.iter_rows():
+                        for cell in row:
+                            # Проверяем, содержит ли ячейка нужное слово
+                            for word in words_list:
+                                if word in str(cell.value):
+                                    print(
+                                        f'Слово "{word}" найдено в ячейке {cell.coordinate} на листе {sheet_name} в файле {file_path}')
+            # Ищем нужные слова в остальных файлах
+            else:
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    for line in file:
+                        # Проверяем, содержит ли строка нужное слово
+                        for word in words_list:
+                            if word in line:
+                                print(f'Слово "{word}" найдено в файле {file_path}')
+
+
+def search_words_in_excel_files(folder_path, words_list):
+    # Перебираем все файлы в папке
+    for root, dirs, files in os.walk(folder_path):
+        for file_name in files:
+            # Получаем полный путь до файла
+            file_path = os.path.join(root, file_name)
+            # Ищем нужные слова в файле Excel
+            if file_name.endswith('.xlsx') or file_name.endswith('.xlsm'):
+                workbook = openpyxl.load_workbook(file_path)
+                for sheet_name in workbook.sheetnames:
+                    sheet = workbook[sheet_name]
+                    for row in sheet.iter_rows():
+                        for cell in row:
+                            # Проверяем, содержит ли ячейка нужное слово
+                            for word in words_list:
+                                if word in str(cell.value):
+                                    print(
+                                        f'Слово "{word}" найдено в ячейке {cell.coordinate} на листе {sheet_name} в файле {file_path}')
