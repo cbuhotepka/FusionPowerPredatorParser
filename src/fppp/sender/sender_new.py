db628b7db4ae303e066cb1a3ec7b595f3288a812
cbuhotepka <cbuhotepka@gmail.com>
2023-06-12 16:49:06 +0300

    Fix port -> host

--- src/fppp/sender/sender_new.py
+++ src/fppp/sender/sender_new.py
@@ -38,7 +38,7 @@ class Colors:
     UNDERLINE = '\033[4m'
 
 
-HOST, PORT = config['FIXPY']['server_port'], config['FIXPY']['server_port']
+HOST, PORT = config['FIXPY']['server_host'], config['FIXPY']['server_port']
 
 
 # combo = None
