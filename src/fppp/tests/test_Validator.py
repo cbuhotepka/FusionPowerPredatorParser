f3f4278004cb2c3d371790a2a72b1af886bb67c6
AlexMay <siberian.may@gmail.com>
2023-06-28 10:27:14 +0200

    feat(validator): Add new hashes to identifier; add test

--- src/fppp/tests/test_Validator.py
+++ src/fppp/tests/test_Validator.py
@@ -2,17 +2,24 @@ import unittest
 from validator.validator import Validator
 from collections import OrderedDict
 
-class ValidatorTest(unittest.TestCase):
 
+class ValidatorTest(unittest.TestCase):
     def setUp(self):
         self.good_line = 'value_username:mail@mail.ru:alter_mail@mail.ru:"P@swwo:rd":+77788899955:+75558886633'
         self.bad_line = 'value_username:mail@mail:alter_mail@mail.ru:P@swword:+77788ff899hj955:+75558886633'
         self.short_line = 'value_username:mail@mail:P@swword:+77788ff899hj955'
         self.keys = [(1, 'username'), (2, 'usermail'), (3, 'usermail'), (4, 'password'), (5, 'tel'), (6, 'tel')]
         self.validator = Validator(keys_and_cols_name=self.keys, num_columns=5, delimiter=':')
-        self.fields = ['value_username', 'mail@mail.ru', 'alter_mail@mail.ru',
-                       '"P@swwo:rd"', '+77788899955', '+75558886633',
-                       'f9a85a5de5de7dea9c9e77a6f511515f', '1b1e15161e15161e110c171a110b16060a170a16']
+        self.fields = [
+            'value_username',
+            'mail@mail.ru',
+            'alter_mail@mail.ru',
+            '"P@swwo:rd"',
+            '+77788899955',
+            '+75558886633',
+            'f9a85a5de5de7dea9c9e77a6f511515f',
+            '1b1e15161e15161e110c171a110b16060a170a16',
+        ]
 
     def test_split_line_to_fields(self):
         good_result = [
@@ -21,7 +28,7 @@ class ValidatorTest(unittest.TestCase):
             'alter_mail@mail.ru',
             'P@swwo,rd',
             '+77788899955',
-            '+75558886633'
+            '+75558886633',
         ]
         result = self.validator.split_line_to_fields(self.good_line)
         self.assertEqual(result, good_result, 'не корректно разбивается строка')
@@ -29,12 +36,7 @@ class ValidatorTest(unittest.TestCase):
         self.assertEqual([], result, 'не корректно разбивается строка')
 
     def test_handler_keys(self):
-        good_result = {
-            'username': [0],
-            'usermail': [1, 2],
-            'password': [3],
-            'tel': [4, 5]
-        }
+        good_result = {'username': [0], 'usermail': [1, 2], 'password': [3], 'tel': [4, 5]}
         result = self.validator.handler_keys()
         self.assertEqual(result, good_result, 'не правильно обработаны ключи')
 
@@ -45,64 +47,66 @@ class ValidatorTest(unittest.TestCase):
         self.assertEqual(good_result, result, 'не правильно очищена строка')
 
     def test_get_fields_good_1(self):
-        good_result = [OrderedDict({
-            'algorithm': '',
-            'username': 'value_username',
-            'usermail': 'mail@mail.ru',
-            'userpass_plain': 'P@swwo,rd',
-            'tel': '+77788899955'
-        }),
-        OrderedDict({
-            'algorithm': '',
-            'username': 'value_username',
-            'usermail': 'alter_mail@mail.ru',
-            'userpass_plain': 'P@swwo,rd',
-            'tel': '+75558886633'
-        })]
+        good_result = [
+            OrderedDict(
+                {
+                    'algorithm': '',
+                    'username': 'value_username',
+                    'usermail': 'mail@mail.ru',
+                    'userpass_plain': 'P@swwo,rd',
+                    'tel': '+77788899955',
+                }
+            ),
+            OrderedDict(
+                {
+                    'algorithm': '',
+                    'username': 'value_username',
+                    'usermail': 'alter_mail@mail.ru',
+                    'userpass_plain': 'P@swwo,rd',
+                    'tel': '+75558886633',
+                }
+            ),
+        ]
         for i, item in enumerate(self.validator.get_fields(self.good_line)):
             self.assertEqual(good_result[i], item, 'не правильно очищена строка')
 
     def test_get_fields_good_2(self):
         good_line = 'user_mail@mail.ru:mail@mail.ru:alter_mail@mail.ru:"P@swwo:rd":+77788899955:+75558886633'
         good_result = [
-            OrderedDict({
-                'algorithm': '',
-                'usermail': 'mail@mail.ru',
-                'userpass_plain': 'P@swwo,rd',
-                'tel': '+77788899955'
-            }),
-            OrderedDict({
-                'algorithm': '',
-                'usermail': 'alter_mail@mail.ru',
-                'userpass_plain': 'P@swwo,rd',
-                'tel': '+75558886633'
-            }),
-            OrderedDict({
-                'algorithm': '',
-                'usermail': 'user_mail@mail.ru',
-                'userpass_plain': 'P@swwo,rd',
-                'tel': '+75558886633'
-            })
+            OrderedDict(
+                {'algorithm': '', 'usermail': 'mail@mail.ru', 'userpass_plain': 'P@swwo,rd', 'tel': '+77788899955'}
+            ),
+            OrderedDict(
+                {
+                    'algorithm': '',
+                    'usermail': 'alter_mail@mail.ru',
+                    'userpass_plain': 'P@swwo,rd',
+                    'tel': '+75558886633',
+                }
+            ),
+            OrderedDict(
+                {'algorithm': '', 'usermail': 'user_mail@mail.ru', 'userpass_plain': 'P@swwo,rd', 'tel': '+75558886633'}
+            ),
         ]
         for i, item in enumerate(self.validator.get_fields(good_line)):
             self.assertEqual(good_result[i], item, 'не правильно очищена строка')
 
     def test_get_fields_bad_1(self):
         bad_line = 'value_username:mail@mail:alter_mail@mail.ru:P@swword:+7g78-8ff899hj955:+75558886633'
-        good_result = [OrderedDict({
-            'algorithm': '',
-            'username': 'value_username',
-            'usermail': '',
-            'userpass_plain': 'P@swword',
-            'tel': ''
-            }),
-            OrderedDict({
-                'algorithm': '',
-                'username': 'value_username',
-                'usermail': 'alter_mail@mail.ru',
-                'userpass_plain': 'P@swword',
-                'tel': '+75558886633'
-            })]
+        good_result = [
+            OrderedDict(
+                {'algorithm': '', 'username': 'value_username', 'usermail': '', 'userpass_plain': 'P@swword', 'tel': ''}
+            ),
+            OrderedDict(
+                {
+                    'algorithm': '',
+                    'username': 'value_username',
+                    'usermail': 'alter_mail@mail.ru',
+                    'userpass_plain': 'P@swword',
+                    'tel': '+75558886633',
+                }
+            ),
+        ]
         for i, item in enumerate(self.validator.get_fields(bad_line)):
             self.assertEqual(good_result[i], item, 'не правильно очищена строка')
 
@@ -114,19 +118,37 @@ class ValidatorTest(unittest.TestCase):
 
 
 class ValidatorTestAutoParse(unittest.TestCase):
-
     def setUp(self):
         self.keys = [(1, 'username'), (2, 'password')]
         self.validator = Validator(keys_and_cols_name=self.keys, num_columns=1, delimiter=':')
 
     def test_get_fields_valid(self):
-        bad_line = 'adaegawet:dagwetawetwear'
-        expected = OrderedDict([('algorithm', ''), ('username', 'adaegawet'), ('userpass_plain', 'dagwetawetwear')])
-        assert list(self.validator.get_fields(bad_line)) == expected, 'Should return result'
+        line = 'random_username:dagwetawetwear'
+        expected = [OrderedDict([('algorithm', ''), ('username', 'random_username'), ('userpass_plain', 'dagwetawetwear')])]
+        result = list(self.validator.get_fields(line))
+        self.assertEqual(result, expected, 'Should return result')
+
+    def test_get_fields_hash(self):
+        for algorithm, hashed_pass in {
+            'MD5': 'f9a85a5de5de7dea9c9e77a6f511515f',
+            'HEX': '$HEX[D23E5DD1FE5059F55E33]',
+            'SHA-1(Hex)': '1b1e15161e15161e110c171a110b16060a170a16',
+        }.items():
+            line = f'random_username:{hashed_pass}'
+            expected = [
+                OrderedDict([('algorithm', algorithm), ('username', 'random_username'), ('hash', hashed_pass)])
+            ]
+            result = list(self.validator.get_fields(line))
+            self.assertEqual(result, expected, 'Should return hash with algorithm')
 
     def test_get_fields_with_domain(self):
-        bad_line = 'https://example'
-        assert list(self.validator.get_fields(bad_line)) == [], 'Username like "https" should return empty list'
+        for domain_line in [
+            'https://example.com',
+            'My Channel:test',
+            'My TG:@telegram',
+        ]:
+            result = list(self.validator.get_fields(domain_line))
+            self.assertEqual(result, [], 'Username like "https" should return empty list')
 
 
 if __name__ == '__main__':
